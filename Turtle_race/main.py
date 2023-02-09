6d4b4030a4fbc90cc589a0e94388bcd73681e18d
TriponMaria <70942541+TriponMaria@users.noreply.github.com>
2023-02-09 22:14:27 +0200

    Add files via upload

---
+++ Turtle_race/main.py
@@ -0,0 +1,11 @@
+with open("../../Desktop/my_file.txt") as file:
+    contents = file.read()
+    print("contents: ", contents)
+
+
+
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nNew text")
+#
+# with open("..\\text.txt", mode="w") as file:
+#     file.write("A\nB")
