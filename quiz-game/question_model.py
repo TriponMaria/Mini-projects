3e1a0cab7cde42765480b949b04477d53a4f23b2
TriponMaria <70942541+TriponMaria@users.noreply.github.com>
2023-02-09 22:12:09 +0200

    Add files via upload

---
+++ quiz-game/question_model.py
@@ -0,0 +1,7 @@
+# Create a Question class with an __init__() method with two attributes:
+# text and answer attributes
+
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
